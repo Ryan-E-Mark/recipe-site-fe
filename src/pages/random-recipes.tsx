d2327cb55e88d102bc3394b3edd3b9d95b7260ca
Ryan Mark <rymark907@gmail.com>
2024-03-25 12:25:45 -0600

    random recipes page styling

--- src/pages/random-recipes.tsx
+++ src/pages/random-recipes.tsx
@@ -12,6 +12,7 @@ import {
 } from "../components/recipe-list/filter-helpers";
 import { getStringCapitalized } from "../utils/get-string-capitalized";
 import { getStringFormatted } from "../utils/get-string-formatted";
+import { useNavigate } from "react-router-dom";
 
 const filterEntities = ["cuisine", "diet"];
 
@@ -37,6 +38,7 @@ export const RandomRecipes = () => {
     diet: false,
   });
   const [tmpFilters, setTmpFilters] = useState<FilterType>(initialFilterState);
+  const navigate = useNavigate();
 
   const filtersByEntity: {
     cuisine: CUISINE_FILTERS_TYPE;
@@ -62,7 +64,7 @@ export const RandomRecipes = () => {
       url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random",
       params: {
         tags: searchParams,
-        number: "3",
+        number: "4",
       },
       headers: {
         "X-RapidAPI-Key": process.env.REACT_APP_RECIPE_SEARCH_API_KEY,
@@ -84,98 +86,96 @@ export const RandomRecipes = () => {
     }
   };
 
+  const handleClick = (id: number) => {
+    navigate(`/recipes/${id}`, { state: { id: id } });
+  };
+
   return (
     <div className="h-screen grid">
       <div className="flex flex-col justify-center items-center self-start">
-        <div className="border-2 bg-gray-200 rounded-md flex flex-col items-center p-2 gap-y-4">
-          <h3 className="text-lg font-bold">Filter your results</h3>
-          <div className="grid grid-cols-2 gap-x-4">
-            {Object.keys(filtersByEntity).map((entity) => {
-              return (
-                <div className="w-auto">
-                  <button
-                    className="inline-flex w-52 justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                    id="menu-button"
-                    aria-expanded="true"
-                    aria-haspopup="true"
-                    onClick={() =>
-                      setIsDropdownOpen({
-                        ...isDropdownOpen,
-                        [entity]:
-                          !isDropdownOpen[
-                            entity as keyof (
-                              | CUISINE_FILTERS_TYPE
-                              | DIET_FILTERS_TYPE
-                            )
-                          ],
-                      })
-                    }
-                  >
-                    {getStringCapitalized(entity)}
-                    <svg
-                      className="-mr-1 h-5 w-5 text-gray-400"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </button>
-                  {isDropdownOpen[
-                    entity as keyof (CUISINE_FILTERS_TYPE | DIET_FILTERS_TYPE)
-                  ] && (
-                    <div className="py-2 my-2 w-52 bg-white drop-shadow-lg rounded-lg overflow-y-scroll h-36 absolute z-10">
-                      <ul>
-                        {Object.keys(
-                          filtersByEntity[
-                            entity as keyof typeof filtersByEntity
-                          ]
-                        ).map((filter, idx) => {
-                          const isChecked = !!tmpFilters[
-                            entity as keyof FilterType
-                          ].includes(
-                            filtersByEntity[
-                              entity as keyof typeof filtersByEntity
-                            ][
-                              filter as keyof (
+        <div className="bg-gray-200 rounded-md flex justify-start gap-x-12 px-4">
+          <div className="flex flex-col p-2 gap-y-4 items-center">
+            <h1 className="text-lg font-bold">Search for random recipes</h1>
+            <button
+              onClick={handleRandomSearch}
+              className="w-3/4 bg-white hover:bg-gray-50 text-black py-1 px-4 rounded-full shadow-sm"
+            >
+              Search
+            </button>
+          </div>
+          <div className="flex flex-col items-center p-2 gap-y-4">
+            <h3 className="text-md font-bold">Filter your results</h3>
+            <div className="grid grid-cols-2 gap-x-4">
+              {Object.keys(filtersByEntity).map((entity) => {
+                return (
+                  <div className="w-auto">
+                    <button
+                      className="inline-flex w-52 justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                      id="menu-button"
+                      aria-expanded="true"
+                      aria-haspopup="true"
+                      onClick={() =>
+                        setIsDropdownOpen({
+                          ...isDropdownOpen,
+                          [entity]:
+                            !isDropdownOpen[
+                              entity as keyof (
                                 | CUISINE_FILTERS_TYPE
                                 | DIET_FILTERS_TYPE
                               )
+                            ],
+                        })
+                      }
+                    >
+                      {getStringCapitalized(entity)}
+                      <svg
+                        className="-mr-1 h-5 w-5 text-gray-400"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                    {isDropdownOpen[
+                      entity as keyof (CUISINE_FILTERS_TYPE | DIET_FILTERS_TYPE)
+                    ] && (
+                      <div className="py-2 my-2 w-52 bg-white drop-shadow-lg rounded-lg overflow-y-scroll h-36 absolute z-10">
+                        <ul>
+                          {Object.keys(
+                            filtersByEntity[
+                              entity as keyof typeof filtersByEntity
                             ]
-                          );
+                          ).map((filter, idx) => {
+                            const isChecked = !!tmpFilters[
+                              entity as keyof FilterType
+                            ].includes(
+                              filtersByEntity[
+                                entity as keyof typeof filtersByEntity
+                              ][
+                                filter as keyof (
+                                  | CUISINE_FILTERS_TYPE
+                                  | DIET_FILTERS_TYPE
+                                )
+                              ]
+                            );
 
-                          return (
-                            <li
-                              key={idx}
-                              className="w-full rounded-t-lg hover:bg-gray-50"
-                              onClick={() => {
-                                if (!isChecked) {
-                                  setTmpFilters({
-                                    ...tmpFilters,
-                                    [entity]: [
-                                      ...tmpFilters[entity as keyof FilterType],
-                                      filtersByEntity[
-                                        entity as keyof typeof filtersByEntity
-                                      ][
-                                        filter as keyof (
-                                          | CUISINE_FILTERS_TYPE
-                                          | DIET_FILTERS_TYPE
-                                        )
-                                      ],
-                                    ],
-                                  });
-                                } else {
-                                  setTmpFilters({
-                                    ...tmpFilters,
-                                    [entity]: tmpFilters[
-                                      entity as keyof FilterType
-                                    ].filter(
-                                      (value) =>
-                                        value !==
+                            return (
+                              <li
+                                key={idx}
+                                className="w-full rounded-t-lg hover:bg-gray-50"
+                                onClick={() => {
+                                  if (!isChecked) {
+                                    setTmpFilters({
+                                      ...tmpFilters,
+                                      [entity]: [
+                                        ...tmpFilters[
+                                          entity as keyof FilterType
+                                        ],
                                         filtersByEntity[
                                           entity as keyof typeof filtersByEntity
                                         ][
@@ -183,81 +183,107 @@ export const RandomRecipes = () => {
                                             | CUISINE_FILTERS_TYPE
                                             | DIET_FILTERS_TYPE
                                           )
-                                        ]
-                                    ),
-                                  });
-                                }
-                              }}
-                            >
-                              <div className="flex items-center ps-3">
-                                <input
-                                  type="checkbox"
-                                  checked={isChecked}
-                                  value={
-                                    filtersByEntity[
-                                      entity as keyof typeof filtersByEntity
-                                    ][
-                                      filter as keyof (
-                                        | CUISINE_FILTERS_TYPE
-                                        | DIET_FILTERS_TYPE
-                                      )
-                                    ]
+                                        ],
+                                      ],
+                                    });
+                                  } else {
+                                    setTmpFilters({
+                                      ...tmpFilters,
+                                      [entity]: tmpFilters[
+                                        entity as keyof FilterType
+                                      ].filter(
+                                        (value) =>
+                                          value !==
+                                          filtersByEntity[
+                                            entity as keyof typeof filtersByEntity
+                                          ][
+                                            filter as keyof (
+                                              | CUISINE_FILTERS_TYPE
+                                              | DIET_FILTERS_TYPE
+                                            )
+                                          ]
+                                      ),
+                                    });
                                   }
-                                  className="w-4 h-4 bg-gray-100 border-gray-300 rounded dark:bg-gray-600 dark:border-gray-500"
-                                />
-                                <label className="text-gray-700 block px-4 py-2 text-sm">
-                                  {getStringFormatted(filter)}
-                                </label>
-                              </div>
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    </div>
-                  )}
-                </div>
-              );
-            })}
-          </div>
-          <div className="w-1/2 flex flex-wrap justify-between">
-            <button
-              onClick={handleClear}
-              className="bg-white hover:bg-gray-50 text-black py-1 px-4 rounded-full"
-            >
-              Clear
-            </button>
-            <button
-              onClick={handleApply}
-              className="bg-white hover:bg-gray-50 text-black py-1 px-4 rounded-full"
-            >
-              Apply
-            </button>
+                                }}
+                              >
+                                <div className="flex items-center ps-3">
+                                  <input
+                                    type="checkbox"
+                                    checked={isChecked}
+                                    value={
+                                      filtersByEntity[
+                                        entity as keyof typeof filtersByEntity
+                                      ][
+                                        filter as keyof (
+                                          | CUISINE_FILTERS_TYPE
+                                          | DIET_FILTERS_TYPE
+                                        )
+                                      ]
+                                    }
+                                    className="w-4 h-4 bg-gray-100 border-gray-300 rounded dark:bg-gray-600 dark:border-gray-500"
+                                  />
+                                  <label className="text-gray-700 block px-4 py-2 text-sm">
+                                    {getStringFormatted(filter)}
+                                  </label>
+                                </div>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-between">
+              <button
+                onClick={handleClear}
+                className="bg-white hover:bg-gray-50 text-black py-1 px-4 rounded-full shadow-sm"
+              >
+                Clear
+              </button>
+              <button
+                onClick={handleApply}
+                className="bg-white hover:bg-gray-50 text-black py-1 px-4 rounded-full shadow-sm"
+              >
+                Apply
+              </button>
+            </div>
           </div>
         </div>
-        <button
-          onClick={handleRandomSearch}
-          className="w-1/6 h-12 border border-solid border-lime-200 hover:bg-lime-200 text-black font-bold py-2 px-4 rounded-full mx-auto"
-        >
-          Search
-        </button>
       </div>
       {isLoading && (
         <div className="flex h-full justify-center items-center">
           <Loader />
         </div>
       )}
-      <div className="flex-col">
-      {!isLoading &&
-        searchResults?.length &&
-        searchResults?.map((result) => {
-          return (
-            <div key={result.id} className="flex-col flex items-center justify-center">
-              <img alt="recipe-thumbnail" src={result.image} className="hover:cursor-pointer rounded-lg"/>
-              <h2 className="hover:cursor-pointer hover:underline font-bold text-xl">{result.title}</h2>
-            </div>
-          );
-        })}
-        </div>
+      <div className="grid grid-cols-2">
+        {!isLoading &&
+          searchResults?.length &&
+          searchResults?.map((result) => {
+            return (
+              <div
+                key={result.id}
+                className="flex-col flex items-center justify-center"
+              >
+                <img
+                  alt="recipe-thumbnail"
+                  src={result.image}
+                  className="hover:cursor-pointer rounded-lg"
+                  onClick={() => handleClick(result?.id)}
+                />
+                <h2
+                  className="hover:cursor-pointer hover:underline font-bold text-xl"
+                  onClick={() => handleClick(result?.id)}
+                >
+                  {result.title}
+                </h2>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
